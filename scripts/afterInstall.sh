641dfad9e559c42ead98bf78a06fa8cbbfeb65cd
Harout Bezdjian <bezdjian-370z@hotmail.com>
2019-10-08 20:17:37 +0200

    new code in afterInstall script

--- scripts/afterInstall.sh
+++ scripts/afterInstall.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/bash
-npm install -g serve
-serve -s build
+
+#npm install -g serve
+#serve -s build
+
+[ -s "/.nvm/nvm.sh" ] && \. "/.nvm/nvm.sh"
+cd /home/ec2-user/
+nohup npm start > /dev/null 2>&1 &
