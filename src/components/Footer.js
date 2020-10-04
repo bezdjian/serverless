9c129d19cf853406786b654831096afcc2b2c97b
Harout Bezdjian <harout.bezdjian@sbab.se>
2020-10-04 12:03:28 +0200

    Remove unused icons in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/js/bootstrap';
 
-import { faHome, faListAlt, faUsers } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
 class Footer extends Component {
   render() {
     return (
