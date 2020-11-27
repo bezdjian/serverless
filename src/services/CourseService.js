fa89abdeb804cbbed631d1859abcad2ae894831c
Harout Bezdjian <harout.bezdjian@sbab.se>
2020-11-27 16:32:59 +0100

    Remove comments

--- src/services/CourseService.js
+++ src/services/CourseService.js
@@ -20,44 +20,43 @@ class CourseService {
   }
 
   saveCourse(course, file) {
+    // Uploading image runs after response? make async? none async?
     if (file) {
-      // TODO: Save the image in S3 (via api->lambda) and retrieve s3 URL and then save into course.imageUrl.
-      console.log('Image uploaded: ', file);
-      console.log('Image uploaded name: ', file.name);
+      console.log('Uploading image: ', file);
       //Add image info into course object
       course.imageName = file.name;
-      this.uploadCourseImage(file, file.name)
-        .then(link => {
-          console.log('LINK: ', link);
-          course.imageUrl = link;
-        })
-        .catch(error => {
-          console.log('Error when getting link: ', error);
-        });
+      var imageUrl = this.uploadCourseImage(file);
     }
+
+    console.log('imageUrl: ', imageUrl);
+    course.imageUrl = imageUrl;
     // When creating a new course, ID is null, so we assign an UUID.
     if (!course.id) course.id = uuid().replace(/-/g, '');
 
+    return this.save(course);
+  }
+
+  uploadCourseImage = async file => {
+    const response = await axios.post(
+      `${UPLOAD_COURSE_IMAGE_URL}/upload?filename=${file.name}`,
+      file,
+      {
+        headers: {
+          'Content-Type': file.type,
+        },
+      },
+    );
+    console.log('S3 RESPONSE: ', response);
+    return response.data.message;
+  };
+
+  save(course) {
+    console.log('Saving course: ', course);
     return axios.post(`${SAVE_COURSE_URL}/save`, JSON.stringify(course), {
-      //return axios.post(`http://localhost:3000/save`, JSON.stringify(course), {
       headers: {
         'Content-Type': 'application/json',
       },
     });
-  }
-
-  uploadCourseImage(file, filename) {
-    return axios
-      .post(`${UPLOAD_COURSE_IMAGE_URL}/upload?filename=${filename}`, file, {
-        headers: {
-          'Content-Type': 'image/png',
-        },
-      })
-      .then(response => {
-        console.log('S3 RESPONSE: ', response);
-        // Return the S3 url link where the image is.
-        return response.data.message;
-      });
   }
 }
 
